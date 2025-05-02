6ad4a945fc93ff33f06f38a0318d2e2ba9e249a7
Lukasz Nogaj <lukanogaj@hotmail.co.uk  git config --global user.name lukanogaj>
2025-05-02 10:20:03 +0100

    return

--- src/components/Action/index.jsx
+++ src/components/Action/index.jsx
@@ -1,30 +1,45 @@
 import styles from "./index.module.scss";
 import data from "../data";
+import arrowUp from "../images/icons/arrow-up.svg";
 
 // Div that include 4 divs (Todays tasks list, todays task with information how many tasks has been done , add new task div, and greeting of support)
 const Action = () => {
 	return (
 		<div className={styles.action}>
 			{/* Today's tasks */}
-			<div className={styles.taskTable}>
-				<h2>Today's Tasks</h2>
-				{data.tasks.map((todoItem) => (
-					<div
-						key={todoItem.id}
-						className={styles.tasks}>
-						<div className={styles.taskIcon}>
-							<img
-								src={todoItem.checkIcon}
-								alt="box"
-							/>
-						</div>
-						<div
-							className={styles.taskText}
-							key={todoItem.id}>
-							{todoItem.task}
-						</div>
+			<div className={styles.taskContainer}>
+				<div className={styles.taskTable}>
+					<div className={styles.taskHeading}>
+						<h2>Today's Tasks</h2>
+						<img
+							src={arrowUp}
+							alt=""
+						/>
 					</div>
-				))}
+					{data.tasks.map((todoItem) => (
+						<div
+							key={todoItem.id}
+							className={styles.tasks}>
+							<div className={styles.taskIcon}>
+								<img
+									src={todoItem.checkIcon}
+									alt="box"
+								/>
+							</div>
+							<div
+								className={styles.taskText}
+								key={todoItem.id}>
+								{todoItem.task}
+							</div>
+						</div>
+					))}
+				</div>
+				<div className={styles.taskCounter}>
+					<div className={styles.taskCounterHeading}>
+						<h2>Today's Tasks</h2>
+						<p>1 out of 4 done</p>
+					</div>
+				</div>
 			</div>
 			<div className={styles.taskCounter}></div>
 			{/* Add new task and greeting  */}
