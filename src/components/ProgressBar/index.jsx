a3804697cf3a03a99e185f87f7a92a835237b3ca
Lukasz Nogaj <lukanogaj@hotmail.co.uk  git config --global user.name lukanogaj>
2025-05-21 11:55:32 +0100

    happy-coding

--- src/components/ProgressBar/index.jsx
+++ src/components/ProgressBar/index.jsx
@@ -6,7 +6,9 @@ const ProgressBar = () => {
 	return (
 		<div className={styles.progressBar}>
 			<div className={styles.circularProgress}>
-				{/* <div className={styles.progressValue}></div> */}
+				<div className={styles.progressValue}>
+					<span>25%</span>
+				</div>
 			</div>
 		</div>
 	);
