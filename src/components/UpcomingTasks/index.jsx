8b9532c07b4d5dd7a65348bf00aa315a6bb4e849
Lukasz Nogaj <lukanogaj@hotmail.co.uk  git config --global user.name lukanogaj>
2025-05-26 13:58:43 +0100

    todos

--- src/components/UpcomingTasks/index.jsx
+++ src/components/UpcomingTasks/index.jsx
@@ -1,9 +1,57 @@
+import { useState } from "react";
+import { useEffect } from "react";
 import styles from "./index.module.scss";
 
 const UpcomingTasks = () => {
+	const [todos, setTodos] = useState([]);
+	const [task, setTask] = useState("");
+
+	// Load TODOs from local storage on app startup
+	useEffect(() => {
+		const storedTodos = JSON.parse(localStorage.getItem("todos"));
+		if (storedTodos) {
+			setTodos(storedTodos);
+		}
+	}, []);
+
+	// Update local storage whenever TODOs change
+	useEffect(() => {
+		localStorage.setItem("todos", JSON.stringify(todos));
+	}, [todos]);
+
+	const handleAddTodo = () => {
+		if (task.trim() !== "") {
+			setTodos([...todos, task]);
+			setTask("");
+		}
+	};
+
+	const handleRemoveTodo = (index) => {
+		const newTodos = todos.filter((_, i) => i !== index);
+		setTodos(newTodos);
+	};
 	return (
 		<div className={styles.upcomingTasks}>
-			<div></div>
+			<header className="App-header">
+				<h1>Upcoming Tasks</h1>
+				<div className="todo-input">
+					<input
+						type="text"
+						placeholder="Add a new task"
+						value={task}
+						onChange={(e) => setTask(e.target.value)}
+					/>
+					<button onClick={handleAddTodo}>Add</button>
+				</div>
+				<ul className="todo-list">
+					{todos.map((todo, index) => (
+						<li key={index}>
+							{todo}
+							<button onClick={() => handleRemoveTodo(index)}>Remove</button>
+						</li>
+					))}
+				</ul>
+			</header>
 		</div>
 	);
 };
