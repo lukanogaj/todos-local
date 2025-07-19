2d8a13967d6ae64cebfdc6e7aafa1756156551b0
Lukasz Nogaj <lukanogaj@hotmail.co.uk  git config --global user.name lukanogaj>
2025-07-19 08:15:15 +0100

    html-content-final

--- src/components/UpcomingTasks/index.jsx
+++ src/components/UpcomingTasks/index.jsx
@@ -1,13 +1,20 @@
 import { useState, useEffect } from "react";
 // import { useEffect } from "react";
 import styles from "./index.module.scss";
+import arrowUp from "../images/arrow.png";
 
 const UpcomingTasks = () => {
 	return (
 		<div className={styles.upcomingTasks}>
 			<div className={styles.upcomingTasksHeader}>
 				<h1 className={styles.headerH1}>Upcoming Tasks</h1>
-				<span>See all</span>
+				<div className={styles.arrow}>
+					<span className={styles.spanArrow}>See all</span>
+					<img
+						src={arrowUp}
+						alt=""
+					/>
+				</div>
 			</div>
 			<div className={styles.tasksContainer}>
 				{/* <ul
